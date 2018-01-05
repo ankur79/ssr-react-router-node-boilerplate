b879b2db40fb5096604fc04d58609c8bbb93cd1b
Agarwal, Ankur <ankur.agarwal@ge.com>
2018-01-05 17:54:15 -0500

    including pug and creating u route

--- app/index.js
+++ app/index.js
@@ -1,12 +1,44 @@
+import { renderToString } from 'react-dom/server'
 import React from 'react';
 import { render } from 'react-dom';
-import {BrowserRouter as Router } from 'react-router-dom';
+import { matchPath, StaticRouter } from 'react-router-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createHistory from 'history/createMemoryHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+
+import reducers from './components/reducers' 
 
 import Main from './components/Main';
 
+
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+    combineReducers({
+    reducers,
+    router: routerReducer
+    }),
+    applyMiddleware(middleware)
+)
+
 render((
-    <Router>
+    <Provider store={store}>
+     <BrowserRouter>
         <Main pokemon={window.__PRELOADED_STATE__}/>
-    </Router>), 
+        </BrowserRouter>
+    </Provider>), 
     document.getElementById('root')
 );
--- build/index.js
+++ build/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _server = require('react-dom/server');
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -8,14 +10,50 @@ var _reactDom = require('react-dom');
 
 var _reactRouterDom = require('react-router-dom');
 
+var _BrowserRouter = require('react-router-dom/BrowserRouter');
+
+var _BrowserRouter2 = _interopRequireDefault(_BrowserRouter);
+
+var _redux = require('redux');
+
+var _reactRedux = require('react-redux');
+
+var _createMemoryHistory = require('history/createMemoryHistory');
+
+var _createMemoryHistory2 = _interopRequireDefault(_createMemoryHistory);
+
+var _reactRouter = require('react-router');
+
+var _reactRouterRedux = require('react-router-redux');
+
+var _reducers = require('./components/reducers');
+
+var _reducers2 = _interopRequireDefault(_reducers);
+
 var _Main = require('./components/Main');
 
 var _Main2 = _interopRequireDefault(_Main);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var history = (0, _createMemoryHistory2.default)();
+
+// Build the middleware for intercepting and dispatching navigation actions
+var middleware = (0, _reactRouterRedux.routerMiddleware)(history);
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+var store = (0, _redux.createStore)((0, _redux.combineReducers)({
+    reducers: _reducers2.default,
+    router: _reactRouterRedux.routerReducer
+}), (0, _redux.applyMiddleware)(middleware));
+
 (0, _reactDom.render)(_react2.default.createElement(
-    _reactRouterDom.BrowserRouter,
-    null,
-    _react2.default.createElement(_Main2.default, { pokemon: window.__PRELOADED_STATE__ })
+    _reactRedux.Provider,
+    { store: store },
+    _react2.default.createElement(
+        _BrowserRouter2.default,
+        null,
+        _react2.default.createElement(_Main2.default, { pokemon: window.__PRELOADED_STATE__ })
+    )
 ), document.getElementById('root'));
